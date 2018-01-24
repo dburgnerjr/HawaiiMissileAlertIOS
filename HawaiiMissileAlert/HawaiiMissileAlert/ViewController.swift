1b4081c2981815a3da366252406165e5c456fe85
Daniel Burgner <dburgnerjr@yahoo.com>
2018-01-23 20:26:10 -0600

    Project init.
    Initial project code setup.

---
+++ HawaiiMissileAlert/HawaiiMissileAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HawaiiMissileAlert
+//
+//  Created by Daniel Burgner on 1/23/18.
+//  Copyright © 2018 Daniel Burgner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
