deb4446572d928f4bd5f85bc6a9c9bd4076d8c75
Daniel Burgner <dburgnerjr@yahoo.com>
2018-01-25 08:50:34 -0600

    D25 Added BMD False Alarm.
    D25 of #100DaysOfCode.  Added BMD False Alarm button with ability to be
    hidden when Missile Alert was not selected.

--- HawaiiMissileAlert/HawaiiMissileAlert/AlertsScreen.swift
+++ HawaiiMissileAlert/HawaiiMissileAlert/AlertsScreen.swift
@@ -13,18 +13,30 @@ class AlertsScreen : UIViewController {
     @IBOutlet weak var btnAmberAlertKauai: UIButton!
     @IBOutlet weak var btnAmberAlertState: UIButton!
     @IBOutlet weak var btnPACOMState: UIButton!
+    @IBOutlet weak var btnBMDFalseAlarm: UIButton!
+
+    var testMessageController: UIAlertController!
+    var confirmationMessageController: UIAlertController!
+    var authorizationMessageController: UIAlertController!
     
     let strAmberAlertKauai = "Amber Alert!  Missing child reported in Kauai County.  License Plate ABC 123."
     let strAmberAlertState = "Amber Alert!  Missing child reported could be anywhere in state.  License Plate ABC 123."
     let strPACOMState = "Missile Alert!  Missiles inbound, seek shelter immediately."
-    
-    var testMessageController: UIAlertController!
-    
+    let strBMDFalseAlarm = "The Missile Alert was a false alarm."
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        btnBMDFalseAlarm.isHidden = true
     }
     
+    
     @IBAction func btnAmberAlertKauai(_ sender: Any) {
+//        confirmationMessageController = UIAlertController(title: "Confirmation", message: "Are you sure?", preferredStyle: UIAlertControllerStyle.alert)
+//        confirmationMessageController.addAction(UIAlertAction(title: "OK", style:
+//            UIAlertActionStyle.default, handler: nil))
+//        confirmationMessageController.addAction(UIAlertAction(title: "Cancel", style:
+//            UIAlertActionStyle.default, handler: nil))
+//        present(confirmationMessageController, animated: true, completion: nil)
         testMessageController = UIAlertController(title: "Amber Alert - Kauai County", message: strAmberAlertKauai, preferredStyle: UIAlertControllerStyle.alert)
         testMessageController.addAction(UIAlertAction(title: "OK", style:
             UIAlertActionStyle.default, handler: nil))
@@ -43,6 +55,14 @@ class AlertsScreen : UIViewController {
         testMessageController.addAction(UIAlertAction(title: "OK", style:
             UIAlertActionStyle.default, handler: nil))
         present(testMessageController, animated: true, completion: nil)
+        btnBMDFalseAlarm.isHidden = false
     }
     
+    @IBAction func btnBMDFalseAlarm(_ sender: Any) {
+        testMessageController = UIAlertController(title: "BMD FALSE ALARM", message: strBMDFalseAlarm, preferredStyle: UIAlertControllerStyle.alert)
+        testMessageController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.default, handler: nil))
+        present(testMessageController, animated: true, completion: nil)
+        btnBMDFalseAlarm.isHidden = true
+    }
 }
